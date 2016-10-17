d76cb7abed1e5c79c8fbc6bb5208dea7ce949ed9
Mark McEver <mark.mcever@vanderbilt.edu>
2016-10-17 10:49:11 -0500

    Changed require order (it  was required after implementing the hooks)

--- classes/ExternalModules.php
+++ classes/ExternalModules.php
@@ -1,11 +1,11 @@
 <?php
 namespace ExternalModules;
 
+require_once __DIR__ . "/AbstractExternalModule.php";
+
 require_once __DIR__ . "/../../redcap_connect.php";
 
 use \Exception;
-
-require_once __DIR__ . "/AbstractExternalModule.php";
 
 class ExternalModules
 {
