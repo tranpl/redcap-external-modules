0b041a94498e4d9397795f43ef36a04b1608e3c7
Mark McEver <mark.mcever@vanderbilt.edu>
2016-10-17 11:11:49 -0500

    Moved the manage link to a new section at the bottom of the control center menu

--- manager/templates/hooks/control_center.php
+++ manager/templates/hooks/control_center.php
@@ -5,14 +5,17 @@ require_once dirname(__FILE__) . '/../../../classes/ExternalModules.php';
 
 <script>
 	$(function () {
-		var link = $('a[href*="ControlCenter/modules_settings.php"]');
-		var newLink = link.clone();
-		newLink.attr('href', '<?=ExternalModules::$BASE_URL?>manager/control_center.php');
-		newLink.html('External Modules');
+		var span = $('<span style="position: relative; float: left; left: 4px;"></span>')
+		span.append('<img src="<?=ExternalModules::getIconPath()?>">')
+		span.append('&nbsp;')
+		span.append('<a href="<?=ExternalModules::$BASE_URL?>manager/control_center.php">Manage External Modules</a>')
+		span.append('<br>')
 
-		link.after(newLink);
-		newLink
-		.before('<br>')
-		.before("<img src='<?=ExternalModules::getIconPath()?>'>&nbsp; ")
+		var menu = $('#control_center_menu')
+		menu.find(':last').remove()
+		menu.append('<div style="clear: both;padding-bottom:6px;margin:0 -6px 3px;border-bottom:1px solid #ddd;"></div>')
+		menu.append('<b style="position:relative;">External Modules</b>')
+		menu.append('<br>')
+		menu.append(span)
 	})
 </script>
