6c869178059d494507874dd7ed51356b995fd740
Mark McEver <mark.mcever@vanderbilt.edu>
2016-10-17 15:44:46 -0500

    Made the projMenuToggle actually work

--- manager/templates/hooks/every_page_top.php
+++ manager/templates/hooks/every_page_top.php
@@ -13,6 +13,32 @@ $project_id = $arguments[0];
 			newPanel.attr('id', 'external_modules_panel')
 			newPanel.find('.x-panel-header div:first-child').html("External Modules")
 
+			// Adapted from base.js lines 7147 to 7170 in v6.16.8
+			newPanel.find('.projMenuToggle').mouseenter(function() {
+				console.log(1)
+				$(this).removeClass('opacity50');
+				if (isIE) $(this).find("img").removeClass('opacity50');
+			}).mouseleave(function() {
+				console.log(2)
+				$(this).addClass('opacity50');
+				if (isIE) $(this).find("img").addClass('opacity50');
+			}).click(function(){ // Copied from base.js line 7155 in v6.16.8
+				var divBox = $(this).parent().parent().find('.x-panel-bwrap:first');
+				// Toggle the box
+				divBox.toggle('blind','fast');
+				// Toggle the image
+				var toggleImg = $(this).find('img:first');
+				if (toggleImg.prop('src').indexOf('toggle-collapse.png') > 0) {
+					toggleImg.prop('src', app_path_images+'toggle-expand.png');
+					var collapse = 1;
+				} else {
+					toggleImg.prop('src', app_path_images+'toggle-collapse.png');
+					var collapse = 0;
+				}
+				// Send ajax request to save cookie
+				$.post(app_path_webroot+'ProjectGeneral/project_menu_collapse.php?pid='+pid, { menu_id: $(this).prop('id'), collapse: collapse });
+			});
+
 			var menubox = newPanel.find('.x-panel-body .menubox .menubox')
 			var exampleLink = menubox.find('.hang:first-child').clone()
 			menubox.html('')
