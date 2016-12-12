254d712f9d49afa224c074e8c5aa3df6ed4c6c44
Mark McEver <mark@fastmail.net>
2016-12-12 10:08:35 -0600

    Updated the sql per Rob's recommendation to make it conisisten with other REDCap tables

--- sql/create tables.sql
+++ sql/create tables.sql
@@ -2,18 +2,18 @@ CREATE TABLE IF NOT EXISTS `redcap_external_modules` (
   `external_module_id` int(11) NOT NULL AUTO_INCREMENT,
   `directory_prefix` varchar(255) NOT NULL,
   PRIMARY KEY (`external_module_id`),
-  UNIQUE KEY `directory_name` (`directory_prefix`)
-);
+  UNIQUE KEY `directory_prefix` (`directory_prefix`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `redcap_external_module_settings` (
   `external_module_id` int(11) NOT NULL,
-  `project_id` int(11),
+  `project_id` int(11) NULL DEFAULT NULL,
   `key` varchar(255) NOT NULL,
   `value` varchar(255) NOT NULL,
   KEY `value` (`value`),
   KEY `key` (`key`),
-  KEY `FK_redcap_external_module_settings_redcap_external_modules` (`external_module_id`),
-  KEY `FK_redcap_external_module_settings_redcap_projects` (`project_id`),
-  CONSTRAINT `FK_redcap_external_module_settings_redcap_external_modules` FOREIGN KEY (`external_module_id`) REFERENCES `redcap_external_modules` (`external_module_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_redcap_external_module_settings_redcap_projects` FOREIGN KEY (`project_id`) REFERENCES `redcap_projects` (`project_id`) ON DELETE CASCADE ON UPDATE CASCADE
-);
+  KEY `external_module_id` (`external_module_id`),
+  KEY `project_id` (`project_id`),
+  FOREIGN KEY (`external_module_id`) REFERENCES `redcap_external_modules` (`external_module_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`project_id`) REFERENCES `redcap_projects` (`project_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
