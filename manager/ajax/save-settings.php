9f5e47f5cd21f82bb8ccabfcae250093e84aa1c3
scottjpearson <scott.j.pearson@vanderbilt.edu>
2017-01-24 15:01:49 -0600

    undo

--- manager/ajax/save-settings.php
+++ manager/ajax/save-settings.php
@@ -4,14 +4,13 @@ require_once '../../classes/ExternalModules.php';
 
 $pid = @$_GET['pid'];
 $moduleDirectoryPrefix = $_GET['moduleDirectoryPrefix'];
-$version = $_GET['version'];
 
 if(empty($pid) && !ExternalModules::hasGlobalSettingsSavePermission($moduleDirectoryPrefix)){
 	die("You don't have permission to save global settings!");
 }
-$config = ExternalModules::getConfig($moduleDirectoryPrefix, $version, $pid);
 
-$files = array();
+$instances = array();    # for repeatable elements, you must save them after the original is saved
+                         # if not, the value is overwritten by a string/int/etc. - not a JSON
 foreach($_POST as $key=>$value){
 	if($value == ''){
 		$value = null;
@@ -25,28 +24,39 @@ foreach($_POST as $key=>$value){
 			die("You don't have permission to save the following project setting: $key");
 		}
 
-                $settings = array();
-                foreach ($config['global-settings'] as $row) {
-                        $settings[] = $row;
-                }
-                foreach ($config['project-settings'] as $row) {
-                        $settings[] = $row;
-                }
-                foreach ($settings as $row) {
-                        if (($key == $row['key']) && $row['type'] && ($row['type'] == "file")) {
-                                $files[$key] = $_FILES[$key];
-                        }
-                }
-
-                if (!$files[$key]) {
+                if (preg_match("/____/", $key)) {
+                        $instances[$key] = $value;
+                } else {
 		        ExternalModules::setProjectSetting($moduleDirectoryPrefix, $pid, $key, $value);
                 }
 	}
 }
 
+# instances must come after the initial settings have been saved
+foreach($instances as $key => $value) {
+        # allow the last match to be blank and not put into the database
+        $last = true;
+        $a = preg_split("/____/", $key);
+        $shortKey = $a[0];
+        $n = $a[1];
+
+        # check if the current element is the last in the repeatable element
+        foreach ($_POST as $key2 => $value2) {
+                $a2 = preg_split("/____/", $key2);
+                if (($a2[0] == $shortKey) && ($a2[1] > $n)) {
+                        $last = false;
+                        break;
+                }
+        }
+
+        # do not put in database if last and value is blank
+        if (!$last || $value != "") { 
+                $a = preg_split("/____/", $key);
+                $data = ExternalModules::setInstance($moduleDirectoryPrefix, $pid, $shortKey, (int) $n, $value);
+        }
+}
+
 header('Content-type: application/json');
 echo json_encode(array(
-        'keys' => json_encode($_POST),
-        'files' => json_encode($files),
 	'status' => 'success'
 ));
