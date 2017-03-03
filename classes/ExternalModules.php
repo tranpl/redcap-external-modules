423ad37d26383ae6a983aaa5ab930376b6633ff5
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2017-03-03 10:14:31 -0600

    Dynamic checking to see which SQL file(s) need to be run

--- classes/ExternalModules.php
+++ classes/ExternalModules.php
@@ -800,7 +800,7 @@ class ExternalModules
 		$projectEnabledDefaults = array();
 
 		// Only attempt to detect enabled modules if the external module tables exist.
-		if(self::areTablesPresent()){
+		if(self::getSqlToRunIfDBOutdated() === ""){
 			$result = self::getSettings(null, null, array(self::KEY_VERSION, self::KEY_ENABLED));
 			while($row = self::validateSettingsRow(db_fetch_assoc($result))){
 				$pid = $row['project_id'];
@@ -840,6 +840,62 @@ class ExternalModules
 		$result = self::query("SHOW TABLES LIKE 'redcap_external_module%'");
 		return db_num_rows($result) > 0;
 	}
+
+	static function isTypePresentInTable()
+	{
+		$sql = "SELECT * 
+			FROM information_schema.COLUMNS 
+			WHERE 
+				TABLE_SCHEMA = '".db_real_escape_string($db)."' 
+				AND TABLE_NAME = 'redcap_external_module_settings' 
+				AND COLUMN_NAME = 'type'";
+
+		$result = self::query($sql);
+		return db_num_rows($result) > 0;
+	}
+
+	# returns SQL statements to be run when the database is outdated.
+	# returns "" if the database is up-to-date
+	#
+	# Checks in order for various conditions
+	# Helper methods in methodName return true if up-to-date; false if out-of-date
+	static function getSqlToRunIfDBOutdated()
+	{
+		$sql = array();
+		$sql[] = array( "file" => "sql/create tables.sql",
+				"methodName" => "areTablesPresent"
+				);
+		$sql[] = array( "file" => "sql/migration-2017-01-18_10-03-00.sql",
+				"methodName" => "isTypePresentInTable"
+				);
+
+		$sqlToReturn = array();
+		foreach ($sql as $row) {
+			$isPresent = self::callPrivateMethod($row['methodName']);
+			if (!$isPresent) {
+				$sqlToReturn[] = htmlspecialchars(file_get_contents(__DIR__ . '/../'.$row['file']));
+			}
+		}
+		return implode("\n", $sqlToReturn);
+	}
+
+        private function callPrivateMethod($methodName)
+        {
+                $args = func_get_args();
+                array_shift($args); // remove the method name
+
+                $class = self::getReflectionClass();
+                $method = $class->getMethod($methodName);
+                $method->setAccessible(true);
+
+                return $method->invokeArgs(null, $args);
+        }
+
+        private function getReflectionClass()
+        {
+                return new \ReflectionClass('ExternalModules\ExternalModules');
+        }
+
 
 	static function addResource($path)
 	{
--- manager/templates/enabled-modules.php
+++ manager/templates/enabled-modules.php
@@ -2,9 +2,10 @@
 namespace ExternalModules;
 require_once dirname(__FILE__) . '/../../classes/ExternalModules.php';
 
-if(!ExternalModules::areTablesPresent()){
-	echo 'Before using External Modules, you must run the following sql to create the appropriate tables:<br><br>';
-	echo '<textarea style="width: 100%; height: 300px">' . htmlspecialchars(file_get_contents(__DIR__ . '/../../sql/create tables.sql')) . '</textarea>';
+$sql = ExternalModules::getSqlToRunIfDBOutdated();
+if($sql !== ""){
+	echo '<p>Your current database table structure does not match REDCap\'s expected table structure for External Modules, which means that database tables and/or parts of tables are missing. Copy the SQL in the box below and execute it in the MySQL database named '.$db.' where the REDCap database tables are stored. Once the SQL has been executed, reload this page to run this check again.</p>';
+	echo '<textarea style="width: 100%; height: 300px" onclick="this.focus();this.select()" readonly="readonly">' . $sql . '</textarea>';
 	return;
 }
 
