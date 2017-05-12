59fac9e1f3a9226de36555bb0f73828ee9834bb2
Kyle McGuffin <kyle.mcguffin@vanderbilt.edu>
2017-05-12 11:26:37 -0400

    Fix improperly merged header includes on control_center and project pages

--- manager/control_center.php
+++ manager/control_center.php
@@ -1,13 +1,8 @@
 <?php
 namespace ExternalModules;
-require_once __DIR__ . '/../classes/ExternalModules.php';
-require_once APP_PATH_DOCROOT . 'ControlCenter/header.php';
 require_once 'templates/shared-header.php';
+require_once APP_PATH_DOCROOT . 'ControlCenter/header.php';
 
-ExternalModules::addResource('css/style.css');
-
-ExternalModules::addResource('https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2-rc.1/css/select2.min.css');
-ExternalModules::addResource('https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2-rc.1/js/select2.min.js');
 ExternalModules::addResource(ExternalModules::getManagerJSDirectory().'globals.js');
 
 ?>
--- manager/project.php
+++ manager/project.php
@@ -1,11 +1,8 @@
 <?php
 namespace ExternalModules;
-require_once __DIR__ . '/../classes/ExternalModules.php';
-require_once ExternalModules::getProjectHeaderPath();
 require_once 'templates/shared-header.php';
+require_once ExternalModules::getProjectHeaderPath();
 
-ExternalModules::addResource('https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2-rc.1/css/select2.min.css');
-ExternalModules::addResource('https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2-rc.1/js/select2.min.js');
 ExternalModules::addResource(ExternalModules::getManagerJSDirectory().'globals.js');
 
 if(!ExternalModules::hasDesignRights()){
