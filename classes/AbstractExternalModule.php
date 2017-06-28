3b96706977c09c2dfdd2f74ecedd746da27be897
Kyle McGuffin <kyle.mcguffin@vanderbilt.edu>
2017-06-28 17:03:30 -0400

    Add passthru functions to AbstractExternalModule.php

--- classes/AbstractExternalModule.php
+++ classes/AbstractExternalModule.php
@@ -234,6 +234,168 @@ class AbstractExternalModule
 		return $this->getConfig()['name'];
 	}
 
+	public function createPassthruForm($projectId,$recordId,$surveyFormName = "", $eventId = "") {
+		list($surveyId,$surveyFormName) = $this->getSurveyId($projectId,$surveyFormName);
+
+		## Validate surveyId and surveyFormName were found
+		if($surveyId == "" || $surveyFormName == "") return false;
+
+		## Find valid event ID for form if it wasn't passed in
+		if($eventId == "") {
+			$eventId = $this->getValidFormEventId($surveyFormName,$projectId);
+
+			if(!$eventId) return false;
+		}
+
+		## Search for a participant and response id for the given survey and record
+		list($participantId,$responseId) = $this->getParticipantAndResponseId($surveyId,$recordId);
+
+		## Create participant and return code if doesn't exist yet
+		if($participantId == "" || $responseId == "") {
+			## Generate a random hash and verify it's unique
+			do {
+				$hash = generateRandomHash(10);
+
+				$sql = "SELECT p.hash
+						FROM redcap_surveys_participants p
+						WHERE p.hash = '$hash'";
+
+				$result = db_query($sql);
+
+				$hashExists = (db_num_rows($result) > 0);
+			} while($hashExists);
+
+			## Insert a participant row for this survey
+			$sql = "INSERT INTO redcap_surveys_participants (survey_id, event_id, participant_email, participant_identifier, hash)
+					VALUES ($surveyId,".prep($eventId).", '', null, '$hash')";
+
+			if(!db_query($sql)) echo "Error: ".db_error()." <br />$sql<br />";
+			$participantId = db_insert_id();
+
+			## Insert a response row for this survey and record
+			$returnCode = generateRandomHash();
+			$firstSubmitDate = "'".date('Y-m-d h:m:s')."'";
+
+			$sql = "INSERT INTO redcap_surveys_response (participant_id, record, first_submit_time, return_code)
+					VALUES ($participantId, ".prep($recordId).", $firstSubmitDate,'$returnCode')";
+
+			if(!db_query($sql)) echo "Error: ".db_error()." <br />$sql<br />";
+			$responseId = db_insert_id();
+		}
+		## Reset response status if it already exists
+		else {
+			# Check if a participant and response exists for this survey/record combo
+			$sql = "SELECT p.hash, r.return_code
+				FROM redcap_surveys_participants p, redcap_surveys_response r
+				WHERE p.survey_id = '$surveyId'
+					AND p.participant_id = r.participant_id
+					AND r.record = '".prep($recordId)."'";
+
+			$row = db_fetch_assoc(db_query($sql));
+			$returnCode = $row['return_code'];
+			$hash = $row['hash'];
+
+			// Set the response as incomplete in the response table
+			$sql = "UPDATE redcap_surveys_participants p, redcap_surveys_response r
+					SET r.completion_time = null, r.first_submit_time = '".date('Y-m-d h:m:s')."'
+					WHERE p.survey_id = $surveyId
+						AND p.event_id = ".prep($eventId)."
+						AND r.participant_id = p.participant_id
+						AND r.record = '".prep($recordId)."' ";
+			db_query($sql);
+		}
+
+		// Set the response as incomplete in the data table
+		$sql = "UPDATE redcap_data
+					SET value = '0'
+					WHERE project_id = ".prep($projectId)."
+						AND record = '".prep($recordId)."'
+						AND event_id = ".prep($eventId)."
+						AND field_name = '{$surveyFormName}_complete'";
+
+		$q = db_query($sql);
+		// Log the event (if value changed)
+		if ($q && db_affected_rows() > 0) {
+			if(function_exists("log_event")) {
+				\log_event($sql,"redcap_data","UPDATE",$recordId,"{$surveyFormName}_complete = '0'","Update record");
+			}
+			else {
+				\Logging::logEvent($sql,"redcap_data","UPDATE",$recordId,"{$surveyFormName}_complete = '0'","Update record");
+			}
+		}
+
+		$surveyLink = APP_PATH_SURVEY_FULL . "?s=$hash";
+
+		@db_query("COMMIT");
+
+		## Build invisible self-submitting HTML form to get the user to the survey
+		echo "<html><body>
+				<form name='passthruform' action='$surveyLink' method='post' enctype='multipart/form-data'>
+				".($returnCode == "NULL" ? "" : "<input type='hidden' value='".$returnCode."' name='__code'/>")."
+				<input type='hidden' value='1' name='__prefill' />
+				</form>
+				<script type='text/javascript'>
+					document.passthruform.submit();
+				</script>
+				</body>
+				</html>";
+		return false;
+	}
+
+	public function getValidFormEventId($formName,$projectId) {
+		if(!is_numeric($projectId)) return false;
+
+		$sql = "SELECT f.event_id
+				FROM redcap_events_forms f, redcap_events_metadata m, redcap_events_arms a
+				WHERE a.project_id = $projectId
+					AND a.arm_id = m.arm_id
+					AND m.event_id = f.event_id
+					AND f.form_name = '".prep($formName)."'
+				ORDER BY f.event_id ASC
+				LIMIT 1";
+
+		$q = db_query($sql);
+
+		if($row = db_fetch_assoc($q)) {
+			return $row['event_id'];
+		}
+
+		return false;
+	}
+
+	public function getSurveyId($projectId,$surveyFormName = "") {
+		// Get survey_id, form status field, and save and return setting
+		$sql = "SELECT s.survey_id, s.form_name, s.save_and_return
+		 		FROM redcap_projects p, redcap_surveys s, redcap_metadata m
+					WHERE p.project_id = ".prep($projectId)."
+						AND p.project_id = s.project_id
+						AND m.project_id = p.project_id
+						AND s.form_name = m.form_name
+						".($surveyFormName != "" ? (is_numeric($surveyFormName) ? "AND s.survey_id = '$surveyFormName'" : "AND s.form_name = '".prep($surveyFormName)."'") : "")
+				."ORDER BY s.survey_id ASC
+				 LIMIT 1";
+
+		$q = db_query($sql);
+		$surveyId = db_result($q, 0, 'survey_id');
+		$surveyFormName = db_result($q, 0, 'form_name');
+
+		return [$surveyId,$surveyFormName];
+	}
+
+	public function getParticipantAndResponseId($surveyId,$recordId) {
+		$sql = "SELECT p.participant_id, r.response_id
+				FROM redcap_surveys_participants p, redcap_surveys_response r
+				WHERE p.survey_id = '$surveyId'
+					AND p.participant_id = r.participant_id
+					AND r.record = '".$recordId."'";
+
+		$q = db_query($sql);
+		$participantId = db_result($q, 0, 'participant_id');
+		$responseId = db_result($q, 0, 'response_id');
+
+		return [$participantId,$responseId];
+	}
+
 	# function to enforce that a pid is required for a particular function
 	private function requireProjectId($pid)
 	{
