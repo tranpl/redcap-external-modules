4ec04b90eaa814cf9db99e972e77c25b26ed2f02
Mark McEver <mark@fastmail.net>
2017-08-09 08:52:43 -0500

    Added checking to ensure that an institution is defined for every author

--- manager/ajax/enable-module.php
+++ manager/ajax/enable-module.php
@@ -18,16 +18,23 @@ else {
     if(empty($config['authors'])){
         $return_data['error_message'] .= "The module named  ".$config['name']." is missing its authors. Fill in the config.json to ENABLE it.<br/>";
     }else{
-        $error_email = true;
+        $missingEmail = true;
         foreach ($config['authors'] as $author){
             if(!empty( $author['email'])){
-                $error_email = false;
+                $missingEmail = false;
                 break;
             }
         }
 
-        if($error_email){
+        if($missingEmail){
             $return_data['error_message'] .= "The module named  ".$config['name']." needs at least one email inside the authors portion of the configuration.  Please fill an email for at least one author in the config.json.<br/>";
+        }
+
+        foreach ($config['authors'] as $author) {
+            if (empty($author['institution'])) {
+                $return_data['error_message'] .= "The module named  " . $config['name'] . " is missing an institution for at least one of it's authors the config.json file.<br/>";
+                break;
+            }
         }
     }
 
